1f9f11b5ad56f9c65e2f25f9343b4fa0a5d369af
Himanshu Patel <ptlhimanshu827@gmail.com>
2017-07-21 00:27:46 -0400

    Initial

---
+++ app/src/main/java/com/example/anshu/qr_gen/QrActivity.java
@@ -0,0 +1,13 @@
+package com.example.anshu.qr_gen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr);
+    }
+}
